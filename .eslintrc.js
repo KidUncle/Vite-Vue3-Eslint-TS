5f96c2880581b13227be40dab9176509816658d0
KidUncle <764393805@qq.com>
2021-07-19 15:49:40 +0800

    测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,37 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/essential",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    // ts的语法检查放在parserOptions里
+    // 'eslint:recommended',
+    // 'plugin:vue/essential',
+    // 'plugin:@typescript-eslint/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["vue", "@typescript-eslint", "prettier"],
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          ...// 保持airbnb-base中的规则不变
+          '**vite**', // ++
+          '**@vitejs**', // ++
+        ],
+        optionalDependencies: false,
+      },
+    ],
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,5 @@ export default defineConfig({
   // 开发服务器配置
   server: {
     port: 11000,
-  }
-})
+  },
+});
